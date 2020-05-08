47f44af047dee03877c5310db3e808b76ea9430d
ayodejid1 <61972920+ayodejid1@users.noreply.github.com>
2020-05-08 22:51:27 +0100

    My C# Task Four Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SqrtDeterminant.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
